7ddc7844d8dee8b5146b5c580e168c38904fe1d5
npaul <npaul@ltu.edu>
2016-10-22 10:07:48 -0400

    Added database controller

--- src/db/DBConnector.java
+++ src/db/DBConnector.java
@@ -4,7 +4,54 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
+/**
+ * A Class used for establishing a connection to the database.
+ * @author npaul
+ *
+ */
 public class DBConnector {
+	private static Connection currentConnection = null; //Current database connection
 
+	/** SQL Database driver */
+	public final static String DB_Driver = "org.gjt.mm.mysql.Driver";
+	
+	/** Database name */
+	public final static String DB_NAME = "robofestnet";
+    
+	/** Database URL */
+	public final static String DB_URL = "jdbc:mysql://localhost:3306/" + DB_NAME;
+	
+	/** Username for accessing database */
+    private final static String USERNAME = "root";
+    
+    /** Password for accessing database */
+    private final static String PASS = "Cassie8876";
+
+	/**
+	 * Check if the <code>currentConnection</code> is null. If so, create a new connection and
+	 * assign it to <code>currentConnection</code>. Return <code>currentConnection</code>.
+	 * This method should only be used within the <code>database</code> package.
+	 * 
+	 */
+     protected static Connection getConnection(){
+        if (currentConnection == null) {
+        	try{
+	        	Class.forName("com.mysql.jdbc.Driver");
+	            currentConnection =  DriverManager.getConnection(DB_URL, USERNAME, PASS);
+	        }catch(SQLException e){
+	            e.printStackTrace();
+	            System.out.println("SQLException: " + e.getMessage());
+	            System.out.println("SQLState: " + e.getSQLState());
+	            System.out.println("VendorError: " + e.getErrorCode());
+	        }catch(ClassNotFoundException e) {
+	            e.printStackTrace();
+	        } 
+        } 
+        
+        return currentConnection;
+    }
+     public static void main(String[] args) {
+    	 getConnection();
+    	 System.out.println("hello world!");
+     }
 }
--- src/db/DBShelter.java
+++ src/db/DBShelter.java
@@ -1,5 +1,52 @@
 package db;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import beans.Shelter;
+
 public class DBShelter {
 	
+	
+	public static void newShelter(Shelter shelter) {
+		try {
+			PreparedStatement ps = DBConnector.getConnection().prepareStatement(
+					"INSERT INTO shelter VALUES(0,?,?,?,?,?,?,?,?,?,?)");
+			
+			ps.setString(1, shelter.getName());
+			ps.setString(2, shelter.getComments());
+			ps.setInt(3, shelter.getTotalBeds());
+			ps.setInt(4, shelter.getTotalBeds()); //Avail beds
+			ps.setDouble(5, shelter.getLat());
+			ps.setDouble(6, shelter.getLon());
+			ps.setString(shelter.getCity());
+			ps.setString(8,shelter.getState());
+			ps.setInt(9, shelter.getZip());
+			ps.setString(10, shelter.getAddress());
+			
+			ps.execute();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/** Update the number of available beds in a shelter */
+	public static void updateCount(int id, int bedCount) {
+		try {
+			PreparedStatement ps = DBConnector.getConnection().prepareStatement(
+					"UPDATE shelter SET availBeds=? WHERE id=?");
+		
+		
+			ps.setInt(1, bedCount);
+			ps.setInt(2, id);
+			
+			ps.executeQuery();
+		
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
