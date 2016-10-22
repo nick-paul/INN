f636328effbb4d17f20aa38b53fb4ed8fb1e7d8f
jcrane <jcrane@MP05VKPV.campus.ltu.edu>
2016-10-22 17:00:42 -0400

    Added methods and variables to ClientControl, Client(bean), and ClientServlet

--- src/beans/Client.java
+++ src/beans/Client.java
@@ -1,5 +1,105 @@
 package beans;
 
 public class Client {
-
+	
+	private int id;
+	private int requestedShelterID;
+	private String firstName;
+	private String lastName;
+	private double lat;
+	private double lon;
+	private String phoneNumber;
+	private int eta;
+	private int beds;
+	private int age;
+	private String gender;
+	private boolean pending;
+	
+	public Client()
+	{
+		id=0;
+		requestedShelterID=0;
+		firstName="";
+		lastName="";
+		lat=0;
+		lon=0;
+		phoneNumber="";
+		eta=0;
+		beds=0;
+		age=0;
+		gender="";
+		pending=true;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getRequestedShelterID() {
+		return requestedShelterID;
+	}
+	public void setRequestedShelterID(int requestedShelterID) {
+		this.requestedShelterID = requestedShelterID;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public double getLat() {
+		return lat;
+	}
+	public void setLat(double lat) {
+		this.lat = lat;
+	}
+	public double getLon() {
+		return lon;
+	}
+	public void setLon(double lon) {
+		this.lon = lon;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public int getEta() {
+		return eta;
+	}
+	public void setEta(int eta) {
+		this.eta = eta;
+	}
+	public int getBeds() {
+		return beds;
+	}
+	public void setBeds(int beds) {
+		this.beds = beds;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public boolean isPending() {
+		return pending;
+	}
+	public void setPending(boolean pending) {
+		this.pending = pending;
+	}
 }
---
+++ src/servlets/ClientServlet.java
@@ -0,0 +1,72 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import controllers.ClientController;
+import controllers.ShelterController;
+public class ClientServlet extends HttpServlet
+{
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ClientServlet() {
+        super();
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        processRequest(request, response);   
+        return;
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        processRequest(request, response);
+        return;
+    }    
+    /*
+     * The processRequest() method is were we will be doing the decision-making
+     * 
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException{    	
+    	
+    	//First, we get the command that the client has sent to the server
+    	String command = request.getParameter("command");
+    	command = command == null ? "" : command;
+    	
+    	
+    	//This is the jsp page that we will forward the user to after we have done 
+    	// any computations that we need
+    	String forwardToPage = "home.jsp";
+    	    		
+    	
+    	//Based on the command, we will decide what to do
+    	switch(command) {
+    	case "newClient":
+    		forwardToPage = ClientController.newClient(request);
+    		break;
+    	//case "updateShelter":
+    		//forwardToPage = ShelterController.updateShelter(request);
+    		//break;
+    	case "getPendingClientsForShelter":
+    		forwardToPage=ClientController.getPendingClients(request);
+    	default:
+    		//Default, return back to the home page
+    		forwardToPage = "home.jsp";
+    	}
+    	
+    	//Redirect the user
+        if(!response.isCommitted()) 
+        {
+            request.getRequestDispatcher(forwardToPage).forward(request, response);
+        }
+        return;
+    }
+}
+
